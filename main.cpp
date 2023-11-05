fd0c5180b2f7113fb780abe594819979a8d8cc0a
Valerio Bonfatti <valerio.bonfatti2@studio.unibo.it>
2023-11-05 05:38:37 +0100

    aggiunti alcuni istogrammi

--- main.cpp
+++ main.cpp
@@ -14,7 +14,17 @@ int main()
     Particle::AddParticleType("k*", 0.89166, 0, 0.050);
     int N = 120;
     Particle EventParticles[N];
-    TH1F *h = new TH1F("h", "abundancies", 7, 0, 1);
+    TCanvas *prova = new TCanvas("prova","Istogrammi fino ad ora");
+    prova->Divide(3,3);
+    
+    TH1F *h1 = new TH1F("h1", "Tipi di particelle generate", 7, 0, 7 ); 
+    TH1F *h2 = new TH1F("h2", "Distribuzione angolo azimutale", 1000, 0, 2 * M_PI);
+    TH1F *h3 = new TH1F("h3", "Distribuzione angolo polare", 1000, 0, M_PI);
+    TH1F *h4 = new TH1F("h4", "Distribuzione impulso",500, 0, 7);
+    TH1F *h5 = new TH1F("h5", "Distribuzione impulso trasverso", 500, 0, 5);
+    TH1F *h6 = new TH1F("h6", "Distribuzione della energia delle particelle", 500, 0, 7);
+    TH1F *h11 = new TH1F("h11", "Massa invariante per k*", 500, 0.6, 1.2);
+    h11->Sumw2();
     for (int j = 0; j < 1e5; ++j)
     {
         for (int i = 0; i < 100; ++i)
@@ -63,9 +73,18 @@ int main()
                     EventParticles[101 + decaycounter].SetIndex(2);
                 }
                 EventParticles[i].Decay2body(EventParticles[100 + decaycounter], EventParticles[101 + decaycounter]);
+                h11->Fill(EventParticles[100+ decaycounter].InvMass(EventParticles[101+ decaycounter]));
                 decaycounter++;
             }
             EventParticles[i].SetP(p * sin(theta) * cos(phi), p * sin(theta) * sin(phi), p * cos(theta));
+            h1->Fill(EventParticles[i]->GetIndex());
+            h2->Fill(phi);
+            h3->Fill(theta);
+            h4->Fill(p);
+            h5->Fill(p*sin(theta));
+            h6->Fill(EventParticles[i]->GetEnergy());
         }
     }
+     
+
 }
