2697f1923070974926106b45b76154c2b56a1fea
PMG2005 <149801162+PMG2005@users.noreply.github.com>
2023-11-10 16:42:16 +0100

    Delete AnglesGen.cpp

--- AnglesGen.cpp
+++
@@ -1,8 +0,0 @@
-TH2F *h =new TH2F("h","",10000,0,Tmath::Pi(),10000,0,2*TMath::Pi());
-
-double_t phi, theta=0;
-for(Int_t i=0; i<nGen; i++){
-     theta=gRandom->Rndm()*TMath::Pi();
-     phi=gRandom->Rndm()*2*TMath::Pi();
-     h->Fill(theta,phi);
-};
